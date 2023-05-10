26543d800349ea4a0b2d8e649031b224c3e1a8c0
Legolas-MK2 <isi_29@tutanota.de>
2023-05-10 20:03:37 +0200

    redesign html and Previous-func

--- app.py
+++ app.py
@@ -58,6 +58,28 @@ def next():
     return redirect('/?idx=' + str(idx))
 
 
+#previous
+@app.route('/previous')
+def previous():
+    # Get the current image or video index from the query parameter
+    idx = request.args.get('idx', default=0, type=int)
+    while True:
+        print(idx)
+        print(image_urls[idx])
+        print()
+        # Increment the index (with wraparound)
+        idx = (idx - 1) % len(image_urls)
+        try:
+            response = requests.head(image_urls[idx])
+            if response.status_code == 200:
+                break
+        except requests.ConnectionError:
+            pass
+
+    # Redirect to the index route with the updated index
+    return redirect('/?idx=' + str(idx))
+
+
 if __name__ == '__main__':
 
     app.run(debug=True)
